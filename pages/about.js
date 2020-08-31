fac234bcac93ecb1f5400f7663af47104940c2c4
Enrique Mejia <enriquemejiacano@gmail.com>
2020-08-31 12:20:47 -0500

    agregando LINK

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const About = () => {
+    return (
+        <div>
+            <Navbar>
+                <p>Esta es la página de about</p>
+            </Navbar>
+        </div>
+    )
+}
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Home = () => {
+    return (
+        <Navbar>
+            <p>Hola Platzi!!</p>
+        </Navbar>
+    )
+}
+
+export default Home
